c4662d6e18de22618b7c8cf33b34943a82ec2cf9
Skqlixur <jmjmbo5@gmail.com>
2022-09-14 02:50:48 +0800

    Moved more stuff around, added redirect, created pages for players, and edited README.md

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rps-game-frontend
 Frontend for two-player rock-paper-scissors game.
+## this is a really bad game as its really easy to cheat
+## also i am shit at css
---
+++ js/script.js
@@ -0,0 +1,15 @@
+let rpsApi = ''
+
+function checkFirstPlayer() {
+  rpsApi = document.getElementById('form').elements['server-input'].value.trim()
+  if (!rpsApi) return alert('please enter a value')
+  if (rpsApi[rpsApi.length - 1] === '/')
+    rpsApi = rpsApi.slice(0, rpsApi.length - 1)
+  axios
+    .get(`${rpsApi}/isFirstPlayer`)
+    .then(data => {
+      if (data.data) window.location.href = 'pages/playerone.html'
+      else window.location.href = 'pages/playertwo.html'
+    })
+    .catch(err => alert(err.response.data))
+}
